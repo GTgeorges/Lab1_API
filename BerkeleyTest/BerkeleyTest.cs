f34f4be79d9fd1c6156140786dcb72a871385908
Masterlau Lau <Masterlau2@hotmail.com>
2022-06-06 01:11:28 -0400

    Added base for test

---
+++ BerkeleyTest/BerkeleyTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BerkeleyTest
+{
+    [TestClass]
+    public class ServerTest
+    {
+        [TestMethod]
+        public void MockTest()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
+
+     }
+}
