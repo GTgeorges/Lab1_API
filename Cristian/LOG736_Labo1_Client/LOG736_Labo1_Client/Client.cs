ebc8b62ac9e4ba8c5ddf7423f72d5ca9ce44a93d
GTgeorges <guillaume.leclaire.1@ens.etsmtl.ca>
2022-06-07 17:44:35 -0400

    Small fixes

--- Cristian/LOG736_Labo1_Client/LOG736_Labo1_Client/Client.cs
+++ Cristian/LOG736_Labo1_Client/LOG736_Labo1_Client/Client.cs
@@ -4,78 +4,79 @@ using System.Text;
 
 namespace LOG736_Labo1
 {
-    public class Client : IClient
-    {
-        private long time;
+	public class Client : IClient
+	{
+		private long time;
 
-        private long accuracy;
+		private long erreur;
 
-        static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+		private DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        public long GetAccuracy()
-        {
-            return accuracy;
-        }
+		public long GetAccuracy()
+		{
+			return erreur;
+		}
 
-        public long GetTime()
-        {
-            return (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
-        }
+		public long GetTime()
+		{
+			return (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
+		}
 
-        public void RequestTime(int serverPort, long currentTime, int numberOftries)
-        {
-            var host = Dns.GetHostEntry("localhost");
-            var ipAddress = host.AddressList[0];
-            var endpoint = new IPEndPoint(ipAddress, serverPort);
-            var counter = 0;
-            Console.WriteLine("Heure courante du client: {0}\n", currentTime);
+		public void RequestTime(int serverPort, long currentTime, int numberOftries)
+		{
+			epoch = epoch.AddMilliseconds((new Random()).Next(-100, 100));
+			var host = Dns.GetHostEntry("localhost");
+			var ipAddress = host.AddressList[0];
+			var endpoint = new IPEndPoint(ipAddress, serverPort);
+			var counter = 0;
+			Console.WriteLine("Heure courante du client: {0}\n", currentTime);
 
-            try
-            {
-                while (counter < numberOftries)
-                {
-                    //Connexion au server d'horloge sur l'ordinateur local
-                    Console.WriteLine("Synchronisation #{0}", ++counter);
-                    Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                    client.Connect(endpoint);
-                    Console.WriteLine("Client connecté à: {0}", client?.RemoteEndPoint?.ToString());
+			try
+			{
+				while (counter < numberOftries)
+				{
+					//Connexion au server d'horloge sur l'ordinateur local
+					Console.WriteLine("Synchronisation #{0}", ++counter);
+					Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+					client.Connect(endpoint);
+					Console.WriteLine("Client connecté à: {0}", client?.RemoteEndPoint?.ToString());
 
-                    //Reception des donnnes du serveur
-                    var requestTime = DateTime.Now.Millisecond;
-                    byte[] messagedReceived = new byte[1024];
-                    int byteRecv = client.Receive(messagedReceived);
-                    var serverTime = Convert.ToInt64(Encoding.ASCII.GetString(messagedReceived, 0, byteRecv));
-                    var responseTime = DateTime.Now.Millisecond;
-                    var realTime = GetTime();
-                    Console.WriteLine("Temps d'horloge retourné par le serveur: {0}", serverTime);
+					//Reception des donnnes du serveur
+					var requestTime = DateTime.Now;
+					byte[] messagedReceived = new byte[1024];
+					int byteRecv = client.Receive(messagedReceived);
+					var serverTime = Convert.ToInt64(Encoding.ASCII.GetString(messagedReceived, 0, byteRecv));
+					var responseTime = DateTime.Now;
+					var realTime = GetTime();
+					Console.WriteLine("Temps d'horloge retourné par le serveur: {0}", serverTime);
 
-                    //Calcul du delais de traitement
-                    var delay = responseTime - requestTime;
-                    Console.WriteLine("Délai de traitement: {0} millisecondes", delay);
+					//Calcul du delais de traitement
+					var delay = (long)(responseTime - requestTime).TotalMilliseconds;
+					Console.WriteLine("Délai de traitement: {0} millisecondes", delay);
 
-                    Console.WriteLine("Heure d'horloge reelle du client: {0}", realTime);
+					Console.WriteLine("Heure d'horloge reelle du client: {0}", realTime);
 
-                    //Ajustement de l'horloge du client
-                    var clientTime = serverTime + TimeSpan.FromMilliseconds(delay / 2).Milliseconds;
-                    Console.WriteLine("Horloge synchronisé du client: {0}", clientTime);
+					//Ajustement de l'horloge du client
+					var clientTime = serverTime + TimeSpan.FromMilliseconds(delay / 2).Milliseconds;
+					Console.WriteLine("Horloge synchronisé du client: {0}", clientTime);
 
-                    //Calcul de l'erreur
-                    accuracy = realTime - clientTime;
-                    Console.WriteLine("Erreur de synchronisation: {0} millisecondes", accuracy);
-                    SetTime(clientTime);
-                    client.Close();
-                    Console.WriteLine();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+					//Calcul de l'erreur
+					erreur = realTime - clientTime;
+					Console.WriteLine("Erreur de synchronisation: {0} millisecondes", erreur);
+					SetTime(erreur);
+					client.Close();
+					Console.WriteLine();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
 
-        public void SetTime(long newTime)
-        {
-            time = newTime;
-        }
-    }
+		public void SetTime(long offset)
+		{
+			epoch = epoch.AddMilliseconds(offset);
+		}
+	}
 }
--- Cristian/LOG736_Labo1_Serveur/LOG736_Labo1_Serveur/Serveur.cs
+++ Cristian/LOG736_Labo1_Serveur/LOG736_Labo1_Serveur/Serveur.cs
@@ -4,69 +4,64 @@ using System.Text;
 
 namespace LOG736_Labo1
 {
-    public class Serveur : IServeur
-    {
-        private List<Socket> sockets;
+	public class Serveur : IServeur
+	{
+		private List<Socket> sockets;
 
-        static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+		private DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        public Serveur()
-        {
-            sockets = new List<Socket>();
-        }
-        public long GetTime()
-        {
-            long ms = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
-            return ms;
-        }
+		public Serveur()
+		{
+			sockets = new List<Socket>();
+		}
+		public long GetTime()
+		{
+			long ms = (long)(DateTime.UtcNow - epoch).TotalMilliseconds;
+			return ms;
+		}
 
-        public void StartServer(int port)
-        {
-            var host = Dns.GetHostEntry("localhost");
-            var ipAddress = host.AddressList[0];
-            var endpoint = new IPEndPoint(ipAddress, port);
-            Socket connection;
-            Random random = new Random();
+		public void StartServer(int port)
+		{
+			epoch = epoch.AddMilliseconds((new Random()).Next(-100, 100));
+			var host = Dns.GetHostEntry("localhost");
+			var ipAddress = host.AddressList[0];
+			var endpoint = new IPEndPoint(ipAddress, port);
+			Socket connection;
+			Random random = new Random();
 
-            try
-            {
-                //Creation du socket serveur et mise en écoute pour les connexions du client
-                Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                sockets.Add(listener);
-                listener.Bind(endpoint);
-                listener.Listen();
-                Console.WriteLine("En attente de connexion...");
+			try
+			{
+				//Creation du socket serveur et mise en écoute pour les connexions du client
+				Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				sockets.Add(listener);
+				listener.Bind(endpoint);
+				listener.Listen();
+				Console.WriteLine("En attente de connexion...");
 
-                while (true)
-                {
-                    //nouvelle connexion au serveur
-                    connection = listener.Accept();
-                    sockets.Add(connection);
+				while (true)
+				{
+					//nouvelle connexion au serveur
+					connection = listener.Accept();
+					sockets.Add(connection);
 
-                    Console.WriteLine("Serveur connecté à {0}.", (connection.LocalEndPoint as IPEndPoint)?.Address);
-                    Console.WriteLine("Appuyez sur Enter pour continuer ou S pour arreter.");
-                    Thread.Sleep(random.Next(50));
-                    connection.Send(Encoding.ASCII.GetBytes(GetTime().ToString()));
+					Console.WriteLine("Serveur connecté à {0}.", (connection.LocalEndPoint as IPEndPoint)?.Address);
+					Console.WriteLine("Appuyez sur Enter pour continuer ou S pour arreter.");
+					Thread.Sleep(random.Next(50));
+					connection.Send(Encoding.ASCII.GetBytes(GetTime().ToString()));
+				}
 
-                    ConsoleKey key = Console.ReadKey(true).Key;
-                    if (key == ConsoleKey.S)
-                        break;
-                    else if (key == ConsoleKey.Enter)
-                        continue;
-                }
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			StopServer();
+		}
 
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            StopServer();
-        }
-
-        public void StopServer()
-        {
-            Console.WriteLine("Arret du serveur...");
-            sockets.ForEach(x => x.Close());
-        }
-    }
+		public void StopServer()
+		{
+			Console.WriteLine("Arret du serveur...");
+			sockets.ForEach(x => x.Close());
+		}
+	}
 }
